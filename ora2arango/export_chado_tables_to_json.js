afed9b4130519945e979b1b1ed8b5114e0698447
Siddhartha Basu <biosidd@gmail.com>
2023-08-14 16:13:16 -0500

    feat(export_chado_tables_to_json.js): add script to export Chado tables to JSON format
    The script exports Chado tables to JSON format using SQLcl. It sets up
    the necessary environment and executes SQL statements to generate JSON
    files for each table. The exported JSON files are saved in the current
    working directory.

---
+++ ora2arango/export_chado_tables_to_json.js
@@ -0,0 +1,30 @@
+use strict
+load("https://raw.githubusercontent.com/lodash/lodash/4.17.15-npm/core.min.js")
+var FileUtils = Java.type("oracle.dbtools.common.utils.FileUtils")
+var Files = Java.type("java.nio.file.Files")
+var Paths = Java.type("java.nio.file.Paths")
+var cwd = FileUtils.getCWD(ctx);
+
+function exportToJson(table) {
+	var spoolStmt = "SPOOL "+"'"+ cwd + table +".json' " + "REPLACE;"
+	sqlcl.setStmt(spoolStmt)
+	sqlcl.run()
+	sqlcl.setStmt("SELECT * FROM " + table + ";")
+	sqlcl.run()
+	sqlcl.setStmt("SPOOL OFF;")
+	sqlcl.run()
+}
+
+sqlcl.setStmt("SET SQLFORMAT json-formatted;")
+sqlcl.run()
+sqlcl.setStmt("SET FEEDBACK off;")
+sqlcl.run()
+sqlcl.setStmt("SET TERMOUT OFF;")
+sqlcl.run()
+_.forEach(Files.readAllLines(Paths.get(args[1])),exportToJson)
+sqlcl.setStmt("SET TERMOUT ON;")
+sqlcl.run()
+sqlcl.setStmt("SET FEEDBACK on;")
+sqlcl.run()
+sqlcl.setStmt("SET SQLFORMAT ansiconsole;")
+sqlcl.run()
