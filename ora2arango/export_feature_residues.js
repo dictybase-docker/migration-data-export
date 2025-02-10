b41d5b50b8204936a5bfab9f972fbaf7d04bb104
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-02-10 17:40:49 -0600

    fix(export_feature_residues.js): remove 'var' declarations to fix compatibility issues with graalvm
    The 'var' declarations are removed to make the script compatible with
    graalvm. Without this change, the script throws an error because the
    variables are already defined.

--- ora2arango/export_feature_residues.js
+++ ora2arango/export_feature_residues.js
@@ -1,9 +1,24 @@
-var FileUtils = Java.type("oracle.dbtools.common.utils.FileUtils")
-var Collectors = Java.type("java.util.stream.Collectors")
-var Files = Java.type("java.nio.file.Files")
-var Paths = Java.type("java.nio.file.Paths")
-var BufferedReader = Java.type("java.io.BufferedReader")
-var StandardOpenOption = Java.type("java.nio.file.StandardOpenOption")
+FileUtils = Java.type("oracle.dbtools.common.utils.FileUtils")
+Collectors = Java.type("java.util.stream.Collectors")
+Files = Java.type("java.nio.file.Files")
+Paths = Java.type("java.nio.file.Paths")
+BufferedReader = Java.type("java.io.BufferedReader")
+StandardOpenOption = Java.type("java.nio.file.StandardOpenOption")
+String = Java.type("java.lang.String")
+
+cwd = FileUtils.getCWD(ctx);
+outputPath = Paths.get(cwd, "/","feature_clob.json")
+Files.writeString(outputPath, "", StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
+
+var stmt = `
+SELECT 
+    feature_id,
+    cvterm.name as feature_type,
+    residues
+FROM feature 
+JOIN cvterm ON cvterm.cvterm_id = feature.type_id
+WHERE residues IS NOT NULL
+`
 
 ret = util.executeReturnList(stmt, {})
 ret.forEach(function(row){
