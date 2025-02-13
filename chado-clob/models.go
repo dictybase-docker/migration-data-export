19ea475cec99444d7c81e7395e46112aba712ad2
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-02-13 14:06:56 -0600

    refactor(chado-clob): return boolean indicating if table was found
    The function `GetStructForTable` now returns a boolean value indicating
    whether the table name was found in the switch statement.

--- chado-clob/models.go
+++ chado-clob/models.go
@@ -3,148 +3,149 @@ package main
 import "strings"
 
 // GetStructForTable returns the appropriate struct type for a given table name
-func GetStructForTable(tableName string) interface{} {
-    switch strings.ToLower(tableName) {
-    case "cvprop":
-        return &Cvprop{}
-    case "featurecvtermprop":
-        return &FeatureCvtermprop{}
-    case "featurepubprop":
-        return &FeaturePubprop{}
-    case "organism":
-        return &Organism{}
-    case "pubprop":
-        return &Pubprop{}
-    case "analysis":
-        return &Analysis{}
-    case "chadologs":
-        return &ChadoLogs{}
-    case "dbxref":
-        return &Dbxref{}
-    case "featureloc":
-        return &Featureloc{}
-    case "cv":
-        return &Cv{}
-    case "feature":
-        return &Feature{}
-    case "pub":
-        return &Pub{}
-    case "arraydesign":
-        return &Arraydesign{}
-    case "chadoprop":
-        return &Chadoprop{}
-    case "cvterm":
-        return &Cvterm{}
-    case "featureprop":
-        return &Featureprop{}
-    case "featurerelationship":
-        return &FeatureRelationship{}
-    case "mvgenedescription":
-        return &MvGeneDescription{}
-    case "phenotype":
-        return &Phenotype{}
-    default:
-        return nil
-    }
+// Returns the struct instance and a boolean indicating if the table was found
+func GetStructForTable(tableName string) (interface{}, bool) {
+	switch strings.ToLower(tableName) {
+	case "cvprop":
+		return &Cvprop{}, true
+	case "featurecvtermprop":
+		return &FeatureCvtermprop{}, true
+	case "featurepubprop":
+		return &FeaturePubprop{}, true
+	case "organism":
+		return &Organism{}, true
+	case "pubprop":
+		return &Pubprop{}, true
+	case "analysis":
+		return &Analysis{}, true
+	case "chadologs":
+		return &ChadoLogs{}, true
+	case "dbxref":
+		return &Dbxref{}, true
+	case "featureloc":
+		return &Featureloc{}, true
+	case "cv":
+		return &Cv{}, true
+	case "feature":
+		return &Feature{}, true
+	case "pub":
+		return &Pub{}, true
+	case "arraydesign":
+		return &Arraydesign{}, true
+	case "chadoprop":
+		return &Chadoprop{}, true
+	case "cvterm":
+		return &Cvterm{}, true
+	case "featureprop":
+		return &Featureprop{}, true
+	case "featurerelationship":
+		return &FeatureRelationship{}, true
+	case "mvgenedescription":
+		return &MvGeneDescription{}, true
+	case "phenotype":
+		return &Phenotype{}, true
+	default:
+		return nil, false
+	}
 }
 
 type Cvprop struct {
-    CvpropID int    // PRIMARY KEY
-    Value    string // from VALUE column
+	CvpropID int    // PRIMARY KEY
+	Value    string // from VALUE column
 }
 
 type FeatureCvtermprop struct {
-    FeatureCvtermpropID int
-    Value               string
+	FeatureCvtermpropID int
+	Value               string
 }
 
 type FeaturePubprop struct {
-    FeaturePubpropID int
-    Value            string
+	FeaturePubpropID int
+	Value            string
 }
 
 type Organism struct {
-    OrganismID int
-    Comment    string `gorm:"column:COMMENT_"`
+	OrganismID int
+	Comment    string `gorm:"column:COMMENT_"`
 }
 
 type Pubprop struct {
-    PubpropID int
-    Value     string
+	PubpropID int
+	Value     string
 }
 
 type Analysis struct {
-    AnalysisID   int
-    Description string
-    Sourceuri   string
+	AnalysisID  int
+	Description string
+	Sourceuri   string
 }
 
 type ChadoLogs struct {
-    ChadoLogsID int
-    NewValue    string
-    OldValue    string
+	ChadoLogsID int
+	NewValue    string
+	OldValue    string
 }
 
 type Dbxref struct {
-    DbxrefID    int
-    Description string
+	DbxrefID    int
+	Description string
 }
 
 type Featureloc struct {
-    FeaturelocID int
-    ResidueInfo  string
+	FeaturelocID int
+	ResidueInfo  string
 }
 
 type Cv struct {
-    CvID       int
-    Definition string
+	CvID       int
+	Definition string
 }
 
 type Feature struct {
-    FeatureID int
-    Residues  string
+	FeatureID int
+	Residues  string
 }
 
 type Pub struct {
-    PubID       int
-    Title       string
-    Volumetitle string
+	PubID       int
+	Title       string
+	Volumetitle string
 }
 
 type Arraydesign struct {
-    ArraydesignID     int
-    ArrayDimensions   string
-    Description       string
-    ElementDimensions string
-    Version          string
+	ArraydesignID     int
+	ArrayDimensions   string
+	Description       string
+	ElementDimensions string
+	Version           string
 }
 
 type Chadoprop struct {
-    ChadopropID int
-    Value       string
+	ChadopropID int
+	Value       string
 }
 
 type Cvterm struct {
-    CvtermID   int
-    Definition string
+	CvtermID   int
+	Definition string
 }
 
 type Featureprop struct {
-    FeaturepropID int
-    Value         string
+	FeaturepropID int
+	Value         string
 }
 
 type FeatureRelationship struct {
-    FeatureRelationshipID int
-    Value                 string
+	FeatureRelationshipID int
+	Value                 string
 }
 
 type MvGeneDescription struct {
-    MvGeneDescriptionID int
-    Description         string
+	MvGeneDescriptionID int
+	Description         string
 }
 
 type Phenotype struct {
-    PhenotypeID int
-    Value       string
+	PhenotypeID int
+	Value       string
 }
